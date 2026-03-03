7332a0895516d0e552945de9874900267e4419f9
Luc Marc KAZADI <77246320+LucMarc7@users.noreply.github.com>
2026-03-03 10:26:13 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -85,3 +85,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {});
+app.use((req, res, next) => {
+  console.log(`Requête reçue: ${req.method} ${req.url}`);
+  next();
+});
